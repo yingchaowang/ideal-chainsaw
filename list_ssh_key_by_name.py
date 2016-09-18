9bef47de3a28c15c2ac7f7b61dbbe823d27a48ae
Fletcher Davis <fletcher.t.davis@gmail.com>
2016-09-17 20:06:36 -0700

    list keys picks all with 'boot' in name now

--- list_ssh_key_by_name.py
+++ list_ssh_key_by_name.py
@@ -3,4 +3,5 @@ from os import environ
 do = DoManager(None, environ["DO_API_TOKEN"], api_version=2)
 from pprint import PrettyPrinter as pp
 p=pp()
-p.pprint([x for x in do.all_ssh_keys() if x['name'] == 'xps 13 bootcamp'])
+#p.pprint([x for x in do.all_ssh_keys() if x['name'] == 'xps 13 bootcamp'])
+p.pprint([x for x in do.all_ssh_keys() if 'boot' in x['name']])
