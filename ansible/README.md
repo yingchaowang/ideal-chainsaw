ddd60bfc6bb5da4f8f581a0d2e25d87c64c2cc24
Fletcher Davis <fletcher.t.davis@gmail.com>
2016-09-17 21:55:13 -0700

    checkin partial jenkins setup...
    going to use jenkins on hypervisor because jenkins in docker is
    confusing and hard to get `docker build .` working well

---
+++ ansible/README.md
@@ -0,0 +1,1 @@
+to get list of do IPs run `ansible --list-hosts all`
--- terraform/mydo.tf
+++ terraform/mydo.tf
@@ -2,6 +2,6 @@ resource "digitalocean_droplet" "mydo" {
     image = "ubuntu-14-04-x64"
     name = "mydo"
     region = "sfo2"
-    size = "1gb"
+    size = "2gb"
     ssh_keys = ["${digitalocean_ssh_key.default.id}"]
 }
