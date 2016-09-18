97d6b137c180ffbce98b9aa250b14ef445dc2f54
Fletcher Davis <fletcher.t.davis@gmail.com>
2016-09-17 23:26:11 -0700

    add beautiful soup example for jenkins task

---
+++ beautiful_soup.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+from collections import Counter
+from operator import itemgetter as iget
+import requests
+
+html = requests.get('http://www.cs.ucla.edu')
+soup = BeautifulSoup(html.text, 'lxml')
+links = Counter()
+for a in soup.find_all('a'):
+    links[a.get('href')] += 1
+
+for v in links.most_common():
+    print v
