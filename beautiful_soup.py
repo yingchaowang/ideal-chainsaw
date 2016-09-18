04828d7095cadfedfc82b52d0be1c089fd76c58d
Fletcher Davis <fletcher.t.davis@gmail.com>
2016-09-18 12:49:37 -0700

    don't count #, prepend url if missing

--- beautiful_soup.py
+++ beautiful_soup.py
@@ -3,11 +3,16 @@ from collections import Counter
 from operator import itemgetter as iget
 import requests
 
-html = requests.get('http://www.cs.ucla.edu')
+url = 'http://www.cs.ucla.edu'
+html = requests.get(url)
 soup = BeautifulSoup(html.text)
 links = Counter()
 for a in soup.find_all('a'):
-    links[a.get('href')] += 1
+    href = a.get('href')
+    if href != '#':
+        if not s.startswith('http')
+            href = url + href
+        links[href] += 1
 
 for v in links.most_common():
     print v
