2612fc2cea1bd8f65eaefacc5fee6ed21818b84f
Fletcher Davis <fletcher.t.davis@gmail.com>
2016-09-17 19:40:25 -0700

    find ssh key

---
+++ list_ssh_key_by_name.py
@@ -0,0 +1,6 @@
+from dopy.manager import DoManager
+from os import environ
+do = DoManager(None, environ["DO_API_TOKEN"], api_version=2)
+from pprint import PrettyPrinter as pp
+p=pp()
+p.pprint([x for x in do.all_ssh_keys() if x['name'] == 'xps 13 bootcamp'])
