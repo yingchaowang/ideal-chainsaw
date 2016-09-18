b20627360d058b3d22d1e586a632851afcab71f2
Fletcher Davis <fletcher.t.davis@gmail.com>
2016-09-17 23:26:01 -0700

    bugfix kill droplets

--- kill_droplets.py
+++ kill_droplets.py
@@ -1,6 +1,7 @@
 from dopy.manager import DoManager
 from os import environ
 do = DoManager(None, environ["DO_API_TOKEN"], api_version=2)
+a=[]
 for x in do.all_active_droplets():
     a.append(x['id'])
 for id in a:
