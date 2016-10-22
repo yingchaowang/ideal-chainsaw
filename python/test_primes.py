f608f2713fa1b21d42e5ff37987ea9768c15595a
Fletcher Davis <fletcher.t.davis@gmail.com>
2016-10-22 15:48:02 -0700

    github trigger jenkins pls

--- python/test_primes.py
+++ python/test_primes.py
@@ -5,7 +5,7 @@ class PrimesTestCase(unittest.TestCase):
     """tests for `primes.py`"""
 
     def test_is_four_prime(self):
-        """is four successfully determined to not be prime?"""
+        """is four determined to not be prime?"""
         self.assertTrue(is_prime(4))
 
     def test_is_five_prime(self):
