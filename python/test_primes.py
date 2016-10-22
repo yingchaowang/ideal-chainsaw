6443bbc1fd266f9c8cee7397fba4376b1f9a83aa
Fletcher Davis <fletcher.t.davis@gmail.com>
2016-10-22 15:43:30 -0700

    commit failling build

--- python/test_primes.py
+++ python/test_primes.py
@@ -4,6 +4,10 @@ from primes import is_prime
 class PrimesTestCase(unittest.TestCase):
     """tests for `primes.py`"""
 
+    def test_is_four_prime(self):
+        """is four successfully determined to not be prime?"""
+        self.assertTrue(is_prime(4))
+
     def test_is_five_prime(self):
         """is five successfully determined to be prime?"""
         self.assertTrue(is_prime(5))
