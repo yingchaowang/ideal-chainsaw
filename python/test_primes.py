6383caa4ec83bcab8bce5e74ec25bfbf8dbe0783
Fletcher Davis <fletcher.t.davis@gmail.com>
2016-10-22 15:49:34 -0700

    github trigger build pls

--- python/test_primes.py
+++ python/test_primes.py
@@ -5,7 +5,7 @@ class PrimesTestCase(unittest.TestCase):
     """tests for `primes.py`"""
 
     def test_is_four_prime(self):
-        """is four determined to not be prime?"""
+        """is four apropriately determined to not be prime?"""
         self.assertTrue(is_prime(4))
 
     def test_is_five_prime(self):
