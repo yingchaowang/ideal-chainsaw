e8707c522b6ec659e3bae9544a3111bb2b83a5a8
Fletcher Davis <fletcher.t.davis@gmail.com>
2016-10-22 15:00:13 -0700

    init unittest

---
+++ webapp/app/test_mything.py
@@ -0,0 +1,12 @@
+import unittest
+from . import simple
+
+class TestMyApp(unittest.TestCase):
+
+    def test_simple(self):
+        res = simple()
+        self.assertTrue('<h1>' in res)
+
+if __name__ == '__main__':
+    unittest.main()
+
