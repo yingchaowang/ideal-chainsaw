bb79d01821120fede26105a8c3d3aba99462b180
yingchaowang <ycwtest-github@yahoo.com>
2016-10-23 12:50:37 -0700

    Update __init__.py

--- webapp/app/__init__.py
+++ webapp/app/__init__.py
@@ -3,17 +3,21 @@ import sqlalchemy
 from flask import Flask, make_response, request, render_template
 app = Flask(__name__)
 
-conn_string = 'postgres://flask:secret@db:5432/flask'
+conn_string = 'postgres://flask:secret@db:5432/public'
 conn = sqlalchemy.create_engine(conn_string, client_encoding='utf8')
 
 @app.route("/")
 def simple():
     return render_template('index.html')
 
-@app.route('/user/<username>')
-def show_user_profile(username):
+@app.route('/users')
+def list_users():
     # show the user profile for that user
-    return 'User %s' % username
+    html = '<h1>The Users</h1>'
+    res = conn.execute ('selecr * from users')
+    for r in res;
+      html +='<br/>' + str(r)
+    return html
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
